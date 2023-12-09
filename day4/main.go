897ee73c472b6472195f001d7c59fa37bfa5828e
adarsh500 <adarshsulegai50@gmail.com>
2023-12-09 21:14:35 +0530

    solve: day4

--- day4/main.go
+++ day4/main.go
@@ -19,8 +19,7 @@ func computePoints(line string) int {
 	count := 0
 	cards := strings.Split(strings.Split(line, ":")[1], "|")
 	winningCards, regularCards := strings.Split(strings.Trim(cards[0], " "), " "), strings.Split(strings.Trim(cards[1], " "), " ")
-	fmt.Println(winningCards)
-	fmt.Println(regularCards)
+
 	for _, cards := range winningCards {
 		cardNum, _ := strconv.Atoi(strings.Trim(cards, " "))
 		counter[cardNum] = 1
@@ -33,14 +32,45 @@ func computePoints(line string) int {
 		}
 
 		if counter[cardNum] == 1 {
-			fmt.Print(cardNum, ",")
 			count++
 		}
 	}
-	fmt.Println()
-	// fmt.Println(counter)
 
 	return int(math.Pow(2, float64(count)-1))
+}
+
+var frequency = map[int]int{}
+
+func getScratchCardsCount(index int, line string) {
+	frequency[index]++
+	counter := map[int]int{}
+	count := 0
+	repetition := frequency[index]
+	cards := strings.Split(strings.Split(line, ":")[1], "|")
+	winningCards, regularCards := strings.Split(strings.Trim(cards[0], " "), " "), strings.Split(strings.Trim(cards[1], " "), " ")
+
+	for _, cards := range winningCards {
+		cardNum, _ := strconv.Atoi(strings.Trim(cards, " "))
+		counter[cardNum] = 1
+	}
+
+	for _, cards := range regularCards {
+		cardNum, _ := strconv.Atoi(strings.Trim(cards, " "))
+		if cardNum == 0 || cards == "" {
+			continue
+		}
+
+		if counter[cardNum] == 1 {
+			count++
+		}
+	}
+
+	fmt.Println("c", count, repetition)
+	for count+index > index {
+		frequency[count+index] += repetition
+		count--
+	}
+
 }
 
 func main() {
@@ -49,8 +79,15 @@ func main() {
 	check(err)
 	lines := strings.Split(string(data), "\n")
 
-	for _, line := range lines {
-		points += computePoints(line)
+	for i, line := range lines {
+		fmt.Println(frequency)
+		getScratchCardsCount(i+1, line)
+	}
+
+	// fmt.Println(frequency)
+
+	for _, value := range frequency {
+		points += value
 	}
 
 	fmt.Println("result", points)
